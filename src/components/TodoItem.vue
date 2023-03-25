1c6559503492c8cbbacab1806803b088d8308de7
Illia Skladnik <scladnik@gmail.com>
2023-03-25 21:09:51 +0200

    TodoItem component added

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,140 +1,68 @@
 <script>
-export default {
-  name: 'TodoItem',
-  props: {
-    todo: Object,
-  },
-  emits: ['update', 'delete'],
-  data() {
-    return {
-      editing: false,
-      newTitle: this.todo.title,
-    };
-  },
-  methods: {
-    toggle() {
-      this.$emit('update', {
-        ...this.todo,
-        completed: !this.todo.completed,
-      });
+  export default {
+    name: 'TodoItem',
+    props: {
+      todo: Object,
     },
-    rename() {
-      if (!this.editing) {
-        return;
+    emits: ['update', 'delete'],
+    methods: {
+      toggle() {
+        this.$emit('update', {
+          ...this.todo,
+          completed: !this.todo.completed,
+        });
+      },
+      remove() {
+        this.$emit('delete');
       }
-
-      this.editing = false;
-
-      if (this.newTitle === this.todo.title) {
-        return;
-      }
-
-      if (this.newTitle === '') {
-        this.remove();
-
-        return;
-      }
-
-      this.$emit('update', {
-        ...this.todo,
-        title: this.newTitle,
-      });
-    },
-    remove() {
-      this.$emit('delete');
-    },
-    edit() {
-      this.newTitle = this.todo.title;
-      this.editing = true;
-
-      this.$nextTick(() => {
-        this.$refs['title-field'].focus();
-      });
     }
-  },
-};
+  }
 </script>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <template>
-  <div class="todo" :class="{ completed: todo.completed }">
-    <label class="todo__status-label">
-      <input
-        type="checkbox"
-        class="todo__status"
-        :checked="todo.completed"
-        @change="toggle"
-      />
-    </label>
+  <div
+    class="todo"
+    :class="{ completed: todo.completed }"
+  >
+  <label class="todo__status-label">
+    <input
+      type="checkbox"
+      class="todo__status"
+      :checked="todo.completed"
+      @change="toggle"
+    />
+  </label>
 
-    <form v-if="editing" @submit.prevent="rename">
-      <input
-        type="text"
-        class="todo__title-field"
-        placeholder="Empty todo will be deleted"
-        v-model.trim="newTitle"
-        ref="title-field"
-        @keyup.esc="editing = false"
-        @blur="rename"
-      />
-    </form>
+  <form v-if="false">
+    <input
+      type="text"
+      class="todo__title-field"
+      placeholder="Empty todo will be deleted"
+      value="Todo is being edited now"
+    />
+  </form>
 
-    <template v-else>
-      <span class="todo__title" @dblclick="edit">
-        {{ todo.title }}
-      </span>
+  <template v-else>
+    <span class="todo__title">{{ todo.title }}</span>
+    <button 
+      @click="remove"
+      class="todo__remove"
+    >
+      x
+    </button>
+  </template>
 
-      <button
-        class="todo__remove"
-        @click="remove"
-      >
-        x
-      </button>
-    </template>
 
-    <div class="modal overlay" :class="{ 'is-active': false }">
-      <div class="modal-background has-background-white-ter"></div>
-      <div class="loader"></div>
-    </div>
+
+  <div
+    class="modal overlay"
+    :class="{ 'is-active': false}"
+  >
+    <div class="modal-background has-background-white-ter"></div>
+    <div class="loader"></div>
   </div>
+</div>
 </template>
 
 <style>
-
 </style>
